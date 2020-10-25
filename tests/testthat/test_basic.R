d6207fc86d336dada045487f321a5677904e4208
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-10-25 19:12:26 -0400

    :construction_worker: call use_cassette() in test_that()

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -1,14 +1,14 @@
 context("Basic unit tests")
-  
-rq1 <- "owl"
-vcr::use_cassette(name = "owl", {
-  res1 <- rowlbot::rowlbot(rq1)
-})
+library(vcr)
 
 test_that("Check one known entry", {
+  vcr::use_cassette(name = "owl", {
+    rq1 <- "owl"
+    res1 <- rowlbot::rowlbot(rq1)
+  })
+  
   expect_equal(class(res1), "rob_def")
   expect_equal(res1$definitions[[1L]]$type, "noun")
   expect_equal(res1$word, rq1)
   expect_equal(res1$pronunciation, "oul")
 })
-  
